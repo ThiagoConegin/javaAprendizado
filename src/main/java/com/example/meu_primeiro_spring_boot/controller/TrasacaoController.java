6f057ad075813800d48ae24524356ddaccd8aeff
Thiago <thiagoconeginconegin@gmail.com>
2025-03-30 13:32:14 -0300

    v1.0

--- src/main/java/com/example/meu_primeiro_spring_boot/controller/TrasacaoController.java
+++ src/main/java/com/example/meu_primeiro_spring_boot/controller/TrasacaoController.java
@@ -20,10 +20,6 @@ public class TrasacaoController {
     @Autowired
     private TransacaoService transacaoService;
 
-    @PostMapping("/transacao")
-    public ResponseEntity<List<TransacaoModel>> transacao(@RequestBody TransacaoModel transacaoModel){
-        return ResponseEntity.ok(transacaoService.recebertrasacao(transacaoModel));
-    }
 
     @DeleteMapping("/del")
     public String deletarRegistros(){
